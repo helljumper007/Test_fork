0b715b958bbf4ab6aeb488fdc40db5e77e683d2f
helljumper007 <arka.ghosh7392@gmail.com>
2024-08-11 00:01:39 +0530

    Basic arithmetic functions

---
+++ pyth-arith.py
@@ -0,0 +1,24 @@
+x = 30
+y = 70
+sum = x+y
+if x > y:
+ diff = x-y
+ div = x/y
+ modu = x%y
+elif y > x:
+ diff = y-x
+ div = y/x
+ modu  = y%x
+else:
+    diff = y-x
+    div = y/x
+    modu  = y%x
+    print ("They're equals")
+
+prod = x * y
+
+print ("sum is ",sum)
+print ("diff is: ",diff)
+print ("prod is: ",prod)
+print ("div is: ",div)
+print ("mod is: ",modu)
