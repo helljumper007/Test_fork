be30834a6a8e20afb787c797d28c6cceda933957
helljumper007 <arka.ghosh7392@gmail.com>
2024-12-13 14:28:21 +0530

    Some mods and practice

--- pyth-arith.py
+++ pyth-arith.py
@@ -1,14 +1,16 @@
-x = 30
-y = 70
+x = 40
+y = 80
 sum = x+y
 if x > y:
- diff = x-y
- div = x/y
- modu = x%y
+    diff = x-y
+    div = x/y
+    modu = x%y
+    print ("x is greater than y")
 elif y > x:
- diff = y-x
- div = y/x
- modu  = y%x
+    diff = y-x
+    div = y/x
+    modu  = y%x
+    print ("y is greater than x")
 else:
     diff = y-x
     div = y/x
@@ -17,7 +19,7 @@ else:
 
 prod = x * y
 
-print ("sum is ",sum)
+print ("sum is ",x+y)
 print ("diff is: ",diff)
 print ("prod is: ",prod)
 print ("div is: ",div)
